3b9ebff3ea1a9c5bc99bd124f658d460dcdb129b
Apestein <ltn2057@protonmail.com>
2023-07-02 22:29:19 -0400

    save point

--- src/env.mjs
+++ src/env.mjs
@@ -6,6 +6,7 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
+  skipValidation: false,
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
 
