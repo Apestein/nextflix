acf0ce9feb945693e6e9531f92304b94bed9a7f0
Apestein <ltn2057@protonmail.com>
2023-07-03 21:36:35 -0400

    save point

--- next.config.mjs
+++ next.config.mjs
@@ -10,17 +10,6 @@ const config = {
   images: {
     domains: ["image.tmdb.org"],
   },
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 }
 
 export default config
