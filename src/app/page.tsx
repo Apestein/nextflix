c9243188b64f6b711899e9a72d2a50de2b31ce3a
Apestein <ltn2057@protonmail.com>
2023-07-03 21:09:08 -0400

    update packages to latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ function pickRandomNowPlayingShow(shows: Show[]) {
 async function getNowPlaying() {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/now_playing?api_key=${env.NEXT_PUBLIC_TMDB_API}&language=en-US&page=1`,
-    { next: { revalidate: 5 } }
+    { next: { revalidate: 0 } }
   )
   if (!res.ok) {
     throw new Error("Failed to fetch data")
@@ -70,7 +70,7 @@ async function getNowPlaying() {
 async function getPopular() {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${env.NEXT_PUBLIC_TMDB_API}&language=en-US&page=1`,
-    { next: { revalidate: 5 } }
+    { next: { revalidate: 0 } }
   )
   if (!res.ok) {
     throw new Error("Failed to fetch data")
@@ -82,7 +82,7 @@ async function getPopular() {
 async function getTopRated() {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/top_rated?api_key=${env.NEXT_PUBLIC_TMDB_API}&language=en-US&page=1`,
-    { next: { revalidate: 5 } }
+    { next: { revalidate: 0 } }
   )
   if (!res.ok) {
     throw new Error("Failed to fetch data")
