a70f89555fc25036a8b55506cb4dc9a5b4a88b1a
Apestein <ltn2057@protonmail.com>
2023-07-18 03:14:01 -0400

    save point

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,10 +1,13 @@
-import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js"
 import { migrate } from "drizzle-orm/postgres-js/migrator"
-import postgres from "postgres"
+import * as schema from "./schema"
+import { neon, neonConfig } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
 import "dotenv/config"
 
-const sql = postgres(process.env.DATABASE_URL ?? "", { max: 1 })
-const db: PostgresJsDatabase = drizzle(sql)
+neonConfig.fetchConnectionCache = true
+
+const sql = neon(process.env.DATABASE_URL ?? "")
+const db = drizzle(sql, { schema })
 
 async function main() {
   await migrate(db, { migrationsFolder: "drizzle" })
@@ -12,3 +15,18 @@ async function main() {
 }
 
 void main()
+
+// import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js"
+// import { migrate } from "drizzle-orm/postgres-js/migrator"
+// import postgres from "postgres"
+// import "dotenv/config"
+
+// const sql = postgres(process.env.DATABASE_URL ?? "", { max: 1 })
+// const db: PostgresJsDatabase = drizzle(sql)
+
+// async function main() {
+//   await migrate(db, { migrationsFolder: "drizzle" })
+//   console.log("migration completed")
+// }
+
+// void main()
