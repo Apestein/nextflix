4171695d44af83960dccb15bb0e1eb8125beba54
Apestein <ltn2057@protonmail.com>
2023-07-29 14:13:11 -0400

    input bug fix

--- src/app/(profile)/manage-profile/[...slug]/page.tsx
+++ src/app/(profile)/manage-profile/[...slug]/page.tsx
@@ -15,7 +15,8 @@ export default function ProfilePage({
   searchParams: { profileId: string }
 }) {
   const [name, setName] = useState(params.slug[0] ?? raise("No params"))
-  const { mutate } = useZact(sa.updateProfile)
+  const { mutate: mutateUpdate } = useZact(sa.updateProfile)
+  const { mutate: mutateDelete } = useZact(sa.deleteProfile)
   const debounced = useDebouncedCallback((value: string) => {
     setName(value)
   }, 500)
@@ -39,20 +40,34 @@ export default function ProfilePage({
       <input
         type="text"
         name="name"
-        value={name}
+        defaultValue={name}
         className="w-full border border-white/40 p-1"
         onChange={(e) => debounced(e.target.value)}
       />
       <section className="space-x-8">
         <Button
-          onClick={() =>
-            void mutate({
+          onClick={() => {
+            void mutateUpdate({
               name: name,
               profileId: searchParams.profileId,
             })
-          }
+            router.replace("/manage-profile")
+            router.refresh()
+          }}
         >
           Update
+        </Button>
+        <Button
+          variant="destructive"
+          onClick={() => {
+            void mutateDelete({
+              profileId: searchParams.profileId,
+            })
+            router.replace("/manage-profile")
+            router.refresh()
+          }}
+        >
+          Delete
         </Button>
       </section>
     </main>
