7c853a5f7ddcdf20cfc10526709e41a99310f9d6
Apestein <ltn2057@protonmail.com>
2023-08-13 00:15:38 -0400

    ForceRefresh

--- src/components/force-refresh.tsx
+++ src/components/force-refresh.tsx
@@ -4,8 +4,9 @@ import { useRef } from "react"
 
 export function ForceFresh() {
   const triedRef = useRef(0)
+  console.log(triedRef.current)
   const router = useRouter()
   triedRef.current += 1
-  if (triedRef.current < 5) setTimeout(() => router.refresh(), 1000)
+  if (triedRef.current < 5) setTimeout(() => router.refresh(), 2000)
   return null
 }
