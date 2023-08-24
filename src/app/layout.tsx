349da628e8966f40e649c574b7ff9ab8f6595249
Apestein <ltn2057@protonmail.com>
2023-08-23 21:06:19 -0400

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,26 +12,10 @@ const siteConfig = {
   name: "Nextflix",
   description:
     "Open source project using bleeding-edge stack. Drizzle ORM + Neon postgres + Clerk auth + Shadcn/ui + everything new in Next.js 13 (server components, server actions, streaming ui, parallel routes, intercepting routes).",
-  url: "https://nextflix-blush.vercel.app/",
 }
 export const metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: ["/og.png"],
-    creator: "@Apestein",
-  },
 }
 
 export default function RootLayout({
